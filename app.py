a033860a41711599c90e2b229adda37744db5d1c
AdcSK8 <69655316+AdcSK8@users.noreply.github.com>
2022-08-10 21:45:14 -0300

    Subindo valores para o html

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from flask import Flask, render_template
 app = Flask("projeto")
 @app.route("/")
 def ola_mundo():
-     return render_template("alo.html"), 200
-
+     nome="Andre Dias"
+     return render_template("alo.html", n=nome), 200
 app.run()
